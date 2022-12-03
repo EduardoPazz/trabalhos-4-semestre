00c1ce1176e8f62baafd8a7ca294dc57b0cef068
Eduardo da Paz <jooeduardo8@gmail.com>
2022-12-03 11:50:59 -0300

    feat: use ReentrantLock in BlockingDB

--- so-ep2/src/main/java/org/example/db/BlockingDB.java
+++ so-ep2/src/main/java/org/example/db/BlockingDB.java
@@ -2,21 +2,33 @@ package org.example.db;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class BlockingDB implements DB {
 
+  private final ReentrantLock lock = new ReentrantLock(true);
   private final List<String> list = new LinkedList<>();
 
   public BlockingDB(List<String> initialElements) {
     list.addAll(initialElements);
   }
 
-  public synchronized void set(int index, String element) {
-    list.set(index, element);
+  public void set(int index, String element) {
+    lock.lock();
+    try {
+      list.set(index, element);
+    } finally {
+      lock.unlock();
+    }
   }
 
-  public synchronized String get(int index) {
-    return list.get(index);
+  public String get(int index) {
+    lock.lock();
+    try {
+      return list.get(index);
+    } finally {
+      lock.unlock();
+    }
   }
 
   public int size() {
