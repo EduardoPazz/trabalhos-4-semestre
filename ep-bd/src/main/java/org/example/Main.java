70fb2b7a4bfd376daf7d6b7fa3fdbb59410905cf
Edu Paz <jooeduardo8@gmail.com>
2022-12-15 21:52:44 -0300

    feat: connect to database

--- ep-bd/src/main/java/org/example/Main.java
+++ ep-bd/src/main/java/org/example/Main.java
@@ -1,12 +1,34 @@
 package org.example;
 
-import org.example.views.MainMenu;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
-  public static void main(String[] args) {
-    var context = new AnnotationConfigApplicationContext(Config.class);
-    var mainMenu = context.getBean(MainMenu.class);
-    mainMenu.run();
+
+  public static void main(String[] args) throws SQLException {
+    testDBConnection();
+
+//
+//    var context = new AnnotationConfigApplicationContext(Config.class);
+//    var mainMenu = context.getBean(MainMenu.class);
+//    mainMenu.run();
+  }
+
+  private static void testDBConnection() throws SQLException {
+    String url = "jdbc:postgresql://localhost/postgres?user=postgres&password=1234";
+    Connection conn = DriverManager.getConnection(url);
+
+    Statement st = conn.createStatement();
+    st.execute("set search_path = \"4.12\";");
+    ResultSet rs = st.executeQuery("SELECT * FROM aluno");
+    while (rs.next()) {
+      System.out.print("Column 1 returned ");
+      System.out.println(rs.getString(1));
+    }
+    rs.close();
+    st.close();
   }
 }
