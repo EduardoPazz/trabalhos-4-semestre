a39e2c6b0a3f999c65a0822719bcd1d0e25756c6
Edu Paz <jooeduardo8@gmail.com>
2022-12-17 19:30:06 -0300

    feat: RegistrationMenu

--- ep-bd/src/main/java/org/example/views/RegistrationMenu.java
+++ ep-bd/src/main/java/org/example/views/RegistrationMenu.java
@@ -1,10 +1,42 @@
 package org.example.views;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Scanner;
+
+import static org.example.views.ViewHelpers.getValidInputWithOptions;
+
 @Component
+@AllArgsConstructor
 class RegistrationMenu {
+
+  private final Scanner scanner;
+
   void run() {
-    System.out.println("NAO IMPLEMENTADO");
+    registrationMenuWhile:
+    while (true) {
+      String chosenOption = getValidInputWithOptions(scanner, """
+          \n*** Conflitos Belicos - Cadastros ***
+          [1] - Cadastrar Divisoes dentro de um Grupo Militar
+          [2] - Cadastrar Conflito Belico
+          [3] - Cadastrar Grupo Militar
+          [4] - Cadastrar Lider Politico
+          [5] - Cadastrar Chefe Militar
+          [Q] - Voltar ao menu anterior
+          """, List.of("1", "2", "3", "4", "5", "Q"));
+
+      switch (chosenOption) {
+        case "1" -> System.out.println("Not implemented yet");
+        case "2" -> System.out.println("Not implemented yet");
+        case "3" -> System.out.println("Not implemented yet");
+        case "4" -> System.out.println("Not implemented yet");
+        case "5" -> System.out.println("Not implemented yet");
+        case "Q" -> {
+          break registrationMenuWhile;
+        }
+      }
+    }
   }
 }
